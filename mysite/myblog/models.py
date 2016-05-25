368a06c255fcf35b35768ed8ca627106258207fc
robertstephen <robertstephenkingsland@yahoo.com>
2016-05-24 23:16:09 -0700

    Updated HW 8

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    
+
     def __str__(self):
         return self.title
 
@@ -23,6 +25,8 @@ class Category(models.Model):
         blank=True,
         related_name='categories'
     )
+    class Meta:
+       verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
