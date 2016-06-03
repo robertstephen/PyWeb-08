44281a400f7f527264e5ef0215786e773cf5dab7
robertstephen <robertstephenkingsland@yahoo.com>
2016-06-02 20:51:22 -0700

    Updated for WK10

--- mysite/myblog/feeds.py
+++ mysite/myblog/feeds.py
@@ -2,6 +2,30 @@ from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from myblog.models import Post
 
+"""
+# Guessing
+
+class feed():
+
+    def __call__(self, *args, **kwargs):
+        request = args[1]
+        response_body = ""
+        # puytting a bunch of meta information into the response body
+
+        item = self.items()
+
+        foreach item as item:
+
+           title = self.item_title(item)
+           description = self.item_)description(item)
+           link = self.item_link(item)
+
+           #put together rss data for that item and append it to the response body
+        #turn the responses body into a full response
+        #return the response
+           
+"""
+
 class PostFeed(Feed):
     title = "Post_RSS"
     link = "/post/feed/"
